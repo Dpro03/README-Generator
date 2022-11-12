6d06b3a59d8ccf679832892151ced5e6a4878d83
David Proto <davidproto@sbcglobal.net>
2022-11-11 20:17:33 -0500

    deleted some redundent html

--- index.js
+++ index.js
@@ -5,31 +5,6 @@ const util = require("util");
 
 const generateMarkdown = require("./utils/generateMarkdown");
 
-/*const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My project is ${title}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;*/
-
-
 // TODO: Create an array of questions for user input
 const questions = [
   {
